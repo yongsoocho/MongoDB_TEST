b61e96cca3fc18f8c579b03e00f7435d626838a7
yongsoocho <yongsoocho578@gmail.com>
2021-03-31 22:55:25 +0000

    2021/04/01/2.2.2

--- client.js
+++ client.js
@@ -1,12 +1,28 @@
 console.log("client code running");
 
 const axios = require('axios');
+const URL = "https://mongodb-test-xisaa.run.goorm.io";
 
 const test = async () => {
 	try{
-		const { data: { blogs } } = await axios.get("https://mongodb-test-xisaa.run.goorm.io/blog");
-		console.log(blogs);
-		
+		let { data } = await axios.get(`${URL}/blog`);
+		console.log(data[0]); // data is blog
+		data = await Promise.all(data.map(async (data) => {
+			// const resUser = await axios.get(`${URL}/${data.user}`);
+			// const resComments = await axios.get(`${URL}/blog/${data._id}/comment`);
+			const [resUser, resComments] = await Promise.all([
+				axios.get(`${URL}/user/${data.user}`), 
+				axios.get(`${URL}/blog/${data._id}/comment`)
+			]);
+			data.user = resUser.data;
+			data.comments = await Promise.all(resComments.data.map(async (comment) => {
+				const resCommentUser = await axios.get(`${URL}/user/${comment.user}`) // data is comment user
+				comment.user = resCommentUser.data;
+				return comment;
+			}));
+			return data;
+		}));
+		console.dir(data[0], { depth: 10 });
 	}catch(err){
 		console.log(err);
 	}
